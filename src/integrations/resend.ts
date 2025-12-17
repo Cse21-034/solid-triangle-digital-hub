38af14ad559547b5ae8a7916f1d822c1fae53460
Cse21-034 <Cse21-034@thuto.bac.ac.bw>
2025-12-17 14:15:52 +0200

    add

---
+++ src/integrations/resend.ts
@@ -0,0 +1,22 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (to: string, subject: string, message: string) => {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to,
+      subject,
+      html: message,
+    });
+
+    if (error) {
+      return { error };
+    }
+
+    return { data };
+  } catch (error) {
+    return { error };
+  }
+};
